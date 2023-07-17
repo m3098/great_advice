2b5ba15d9f810a98c750f37df6abf9e6cf6a583c
limnull <m.3098.off@gmail.com>
2023-07-17 09:09:31 +0300

    базовая логика приложения

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,13 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:great_advice/repositories/great_advice_repository.dart';
+import 'package:great_advice/repositories/models/advice_model.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final List<AdviceModel> adviceList = [];
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+    on<AddAdviceInList>((event, emit) async {
+      emit(HomeLoading());
+
+      try {
+        adviceList.add(await GreatAdviceRepository().getRandomAdvice());
+        emit(HomeLoaded(adviceList: adviceList));
+      } catch (e) {
+        emit(HomeLoadedFailed());
+      }
     });
   }
 }
--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -2,9 +2,22 @@ part of 'home_bloc.dart';
 
 abstract class HomeState extends Equatable {
   const HomeState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeLoaded extends HomeState {
+  final List<AdviceModel> adviceList;
+
+  const HomeLoaded({required this.adviceList});
+
+  @override
+  List<Object> get props => [adviceList];
+}
+
+class HomeLoadedFailed extends HomeState {}
